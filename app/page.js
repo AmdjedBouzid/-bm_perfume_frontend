0ebabde3618775fc3b4c57fd5f0acfd01609a1d1
Amdjed <amdjedbouzid9@gmail.com>
2025-02-10 23:56:53 +0100

    complete images exporting

--- app/page.js
+++ app/page.js
@@ -2,12 +2,16 @@
 import Image from "next/image";
 import { useAppContext } from "./context/AppContext";
 import { DOMAIN } from "./utils/constants";
+import images from "../public/assets";
 export default function Home() {
   const { admin } = useAppContext();
   console.log(DOMAIN);
   console.log("admin", admin);
-  return <div className="">
-
-    <h1 className="text-3xl flex justify-center mt -4 font-extrabold">مرحبا بك</h1>
-  </div>;
+  return (
+    <div className="">
+      <h1 className="text-3xl flex justify-center mt -4 font-extrabold">
+        مرحبا بك
+      </h1>
+    </div>
+  );
 }
